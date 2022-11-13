db3fd10d1f56871d9edee8292ade3834b24c38c2
namaridamaX <b2200170@photon.chitose.ac.jp>
2022-11-13 23:23:48 +0900

    WebPageを表示する02

--- src/main/java/com/example/wsbp/page/HomePage.java
+++ src/main/java/com/example/wsbp/page/HomePage.java
@@ -4,11 +4,16 @@ import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
 import org.apache.wicket.markup.html.WebPage;
 import org.wicketstuff.annotation.mount.MountPath;
 
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.Model;
+
 @WicketHomePage
 @MountPath("Home")
 
 public class HomePage extends WebPage {
     public HomePage(){
-
+        var youModel= Model.of("Wicket-Spring-Boot");
+        var youLabel = new Label("you",youModel);
+        add(youLabel);
     }
 }
