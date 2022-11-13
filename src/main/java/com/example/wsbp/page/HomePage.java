46c14460dc368955b08701e2e7cbc6469a15e80d
namaridamaX <b2200170@photon.chitose.ac.jp>
2022-11-13 23:35:21 +0900

    WebPage èを表示する 課題

--- src/main/java/com/example/wsbp/page/HomePage.java
+++ src/main/java/com/example/wsbp/page/HomePage.java
@@ -15,5 +15,13 @@ public class HomePage extends WebPage {
         var youModel= Model.of("Wicket-Spring-Boot");
         var youLabel = new Label("you",youModel);
         add(youLabel);
+
+        var gakuseki_model = Model.of("b2200170");
+        var gakuseki = new Label("gakuseki",gakuseki_model);
+        add(gakuseki);
+
+        var name_model = Model.of("新木魁");
+        var name = new Label("name",name_model);
+        add(name);
     }
 }
