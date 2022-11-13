a3b46e115d2354eb3bc22bd971b0e684e126d511
namaridamaX <b2200170@photon.chitose.ac.jp>
2022-11-13 23:12:55 +0900

    WebPageを表ã表示する

---
+++ src/main/java/com/example/wsbp/page/HomePage.java
@@ -0,0 +1,14 @@
+package com.example.wsbp.page;
+
+import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
+import org.apache.wicket.markup.html.WebPage;
+import org.wicketstuff.annotation.mount.MountPath;
+
+@WicketHomePage
+@MountPath("Home")
+
+public class HomePage extends WebPage {
+    public HomePage(){
+
+    }
+}
