87d6171de40fb356169c80107d3fe3929f1afae7
namaridamaX <b2200170@photon.chitose.ac.jp>
2022-11-30 01:23:43 +0900

    ユーザ認証を作成する（Wicket-Auth-Roles） 02

---
+++ src/main/java/com/example/wsbp/MySession.java
@@ -0,0 +1,56 @@
+package com.example.wsbp;
+
+import org.apache.wicket.Session;
+import org.apache.wicket.authroles.authentication.AbstractAuthenticatedWebSession;
+import org.apache.wicket.authroles.authorization.strategies.role.Roles;
+import org.apache.wicket.request.Request;
+
+import java.util.Objects;
+
+// ブラウザ事に異なる情報を一時保存できるセッション
+public class MySession extends AbstractAuthenticatedWebSession{
+
+    //認証の際に照合したユーザ名
+    private String userName;
+
+    public MySession(Request request){
+        // コンストラクタ。初期状態は認証NG (userName = null)
+        super(request);
+        this.userName = null;
+    }
+
+    public void sign(String userName){
+        //認証したユーザを変更する
+        //セキュリティの配慮から、セッションを切り替える（古いセッションを使い回さない）
+        replaceSession();
+        this.userName = userName;
+    }
+
+    //認証したユーザの役割を返り値にするメソッド。Wicketはこの値を使って、ページを表示していいのかを判定する
+    @Override
+    public Roles getRoles(){
+        //認証結果OK(userName = ユーザ名)だった場合は、誰もが"USER"権限を持っているとして返す
+        //認証結果NG(userName = null)だった場合は、権限なしとして返す
+        //wicket-auth-roles では、この権限の有無と種類でページを表示して良いか判断してくれる
+        if(isSignedIn()){
+            return new Roles(Roles.USER);
+        }
+        return new Roles();
+    }
+
+    @Override
+    public boolean isSignedIn(){
+        // 認証結果OK(userName = ユーザー名)だった場合は、trueを返す
+        // 認証結果NG(userName = null）だった場合は、falseを返す
+        return Objects.nonNull(this.userName);
+    }
+
+    public String getUserName(){
+        return this.userName;
+    }
+
+    public static MySession get(){
+        // サーバーの中から Session を取り出す
+        return (MySession) Session.get();
+    }
+}
--- src/main/java/com/example/wsbp/page/UserMakerPage.java
+++ src/main/java/com/example/wsbp/page/UserMakerPage.java
@@ -5,6 +5,7 @@ import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.validation.validator.StringValidator;
 import org.wicketstuff.annotation.mount.MountPath;
 
 import org.apache.wicket.model.Model;
@@ -44,10 +45,28 @@ public class UserMakerPage extends WebPage {
         };
         add(userInfoForm);
 
-        var userNameField = new TextField<>("userName", userNameModel);
+        var userNameField = new TextField<>("userName", userNameModel){
+            // onInitialize() は全てのコンポーネントに備わっている、初期化時の処理。
+            // オーバーライドするときは super.onInitialize() を忘れずに残しておく。
+            @Override
+            protected void onInitialize(){
+                super.onInitialize();
+                // 文字列の長さを8〜32文字に制限するバリデータ
+                var validator = StringValidator.lengthBetween(8,32);
+                add(validator);
+            }
+        };
         userInfoForm.add(userNameField);
 
-        var userPassField = new PasswordTextField("userPass", userPassModel);
+        var userPassField = new PasswordTextField("userPass", userPassModel){
+
+          @Override
+          protected void onInitialize(){
+              super.onInitialize();
+              var validator = StringValidator.lengthBetween(8,32);
+              add(validator);
+          }
+        };
         userInfoForm.add(userPassField);
 
     }
